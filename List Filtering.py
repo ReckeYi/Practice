f8e514e254f28ce3fd6b379aa946d823e3031b94
ReckeYi <79193494+ReckeYi@users.noreply.github.com>
2023-10-13 02:32:24 +0300

    + List Filtering + You're a square!

---
+++ List Filtering.py
@@ -0,0 +1,31 @@
+"""
+List Filtering
+
+DESCRIPTION:
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+"""
+
+
+# My solution
+def filter_list(lst):
+    return [i for i in lst if type(i) == int]
+
+
+"""
+Best Practices
+
+#1
+def filter_list(l):
+  'return a new list with the strings filtered out'
+  return [i for i in l if not isinstance(i, str)]
+  
+#2
+def filter_list(l):
+  'return a new list with the strings filtered out'
+  return [x for x in l if type(x) is not str]
+"""
